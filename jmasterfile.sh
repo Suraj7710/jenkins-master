4c75f1e1635d72283b52b405eb5b8caa54eb15d0
Suraj7710 <123540185+Suraj7710@users.noreply.github.com>
2023-05-22 14:17:43 +0530

    Update jmasterfile.sh

--- jmasterfile.sh
+++ jmasterfile.sh
@@ -6,7 +6,7 @@ cd /mnt
 wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.75/bin/apache-tomcat-9.0.75.zip
 unzip apache-tomcat-9.0.75.zip
 rm -rf apache-tomcat-9.0.75.zip
-cd /apache-tomcat-9.0.75/webapps
+cd /mnt/apache-tomcat-9.0.75/webapps
 wget https://get.jenkins.io/war-stable/2.346.3/jenkins.war
 cd /mnt/apache-tomcat-9.0.75/bin
 chmod -R 777 *
